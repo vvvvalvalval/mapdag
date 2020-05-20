0de1354f7a6027f71f10bfe8b90250d902baf2a8
Valentin Waeselynck <vwaeselynck@ap2s.fr>
2020-05-20 17:12:52 +0200

    Removed test deps in artifact and bumped version to 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject vvvvalvalval/mapdag "0.1.0-alpha0"
+(defproject vvvvalvalval/mapdag "0.1.0"
   :description "A library for expressing computations as DAGs of named steps. Maps-in, maps-out."
   :url "https://github.com/vvvvalvalval/mapdag"
   :license {:name "MIT"
@@ -6,8 +6,4 @@
   :plugins [[lein-tools-deps "0.4.5"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
   :lein-tools-deps/config {:config-files [:project]
-                           :aliases []}
-  :profiles
-  {:test
-   {:lein-tools-deps/config
-    {:aliases [:test]}}})
+                           :aliases []})
